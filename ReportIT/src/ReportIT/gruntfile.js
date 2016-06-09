2037283b110ac7fedc38401ebac70dad121f5f16
Yuriy Ermolenko <yuriy.ermolenko@nitka.com>
2016-06-09 15:46:32 +0300

    Copy bower_components to wwwroot\lib

--- ReportIT/src/ReportIT/gruntfile.js
+++ ReportIT/src/ReportIT/gruntfile.js
@@ -1,3 +1,23 @@
 ﻿module.exports = function (grunt) {
-   
+    grunt.initConfig({
+        clean: ["wwwroot/lib/"],
+        copy: {
+            main: {
+                files: [
+                    {
+                        src: "bower_components/angular/angular.js",
+                        dest: "wwwroot/lib/angular/",
+                        expand: true,
+                        filter: "isFile",
+                        flatten: true
+                    }
+                ]
+            }
+        }
+    });
+
+    grunt.loadNpmTasks("grunt-contrib-clean");
+    grunt.loadNpmTasks("grunt-contrib-copy");
+    grunt.registerTask("all", ["clean", "copy"]);
+    grunt.registerTask("default", "all");
 };
