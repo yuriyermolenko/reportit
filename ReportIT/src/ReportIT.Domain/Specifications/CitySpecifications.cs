b383021a79d7f960798dab01d75b1d7f29329250
Yuriy Ermolenko <yuriy.ermolenko@gmail.com>
2016-07-13 11:05:57 +0300

    Specification implementation sample

---
+++ ReportIT/src/ReportIT.Domain/Specifications/CitySpecifications.cs
@@ -0,0 +1,18 @@
+﻿using ReportIT.Domain.Aggregates.CityAgg;
+using ReportIT.Domain.Specifications.Base;
+
+namespace ReportIT.Domain.Specifications
+{
+    public static class CitySpecifications
+    {
+        public static Specification<City> ById(int cityId)
+        {
+            return new DirectSpecification<City>(city => city.Id == cityId);
+        }
+
+        public static Specification<City> ByName(string name)
+        {
+            return new DirectSpecification<City>(city => city.Name == name);
+        }
+    }
+}
